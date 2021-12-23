dc9a4adf13c4738de393ed20b55d9c26cb59e912
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-12-23 14:34:50 -0500

    added color pink and set Header background to pink when Platform is 'ios'

--- themes/themes.js
+++ themes/themes.js
@@ -1,24 +1,24 @@
-export default { 
-    colors:{
-        mainBlack: '#222',
-        black: '#000011',
-        mainBackground: '#f0f8ff',
-        orange: '#ffa500',
-        olivedrab: '#6b8e23',
-        startButton: '#4630EB',
-        darkerOrange: '#f86E00',
-        brightBlue: 'blue',
-        navyBlue: '#1A2856',
-        orangeSyneos: '#EB3301',
-        gray: '#1A2856',
-        ligtherGray: '#EAF3F8'
-    },
+export default {
+  colors: {
+    mainBlack: "#222",
+    black: "#000011",
+    mainBackground: "#f0f8ff",
+    orange: "#ffa500",
+    olivedrab: "#6b8e23",
+    startButton: "#4630EB",
+    darkerOrange: "#f86E00",
+    brightBlue: "blue",
+    navyBlue: "#1A2856",
+    orangeSyneos: "#EB3301",
+    gray: "#1A2856",
+    ligtherGray: "#EAF3F8",
+    pink: "#f72287",
+  },
 
-    fonts:{
-        SansRegular: "open-sans-regular",
-        SansBold: "open-sans-regular",
-        MontSerratRegular: 'montserrat-regular',
-        MontSerratSemiBold: 'montserrat-semi-bold'
-    }
-
-}
+  fonts: {
+    SansRegular: "open-sans-regular",
+    SansBold: "open-sans-regular",
+    MontSerratRegular: "montserrat-regular",
+    MontSerratSemiBold: "montserrat-semi-bold",
+  },
+};
